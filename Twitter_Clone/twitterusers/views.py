5e7b261b24d0fb1615a562104c6b061e6683da45
Unknown <keybladeschoosen1@gmail.com>
2020-02-12 13:42:19 -0500

    Added user page view

--- Twitter_Clone/twitterusers/views.py
+++ Twitter_Clone/twitterusers/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+from Twitter_Clone.tweets.models import Tweet
+from Twitter_Clone.twitterusers.models import TwitterUser
 
 
 def twitter_user_view(request, id):
-    return HttpResponse(f"user is {id}")
+    twitter_user = TwitterUser.objects.get(pk=id)
+    tweets = Tweet.objects.filter(user=twitter_user)
+    return render(request, 'user.html', {'twitter_user': twitter_user, 'tweets': tweets})
